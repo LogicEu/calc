db7e78f2ead40877b03bfce17acc2b09f1cd5956
LogicEu <eugenioarteagaa@gmail.com>
2023-08-27 18:37:52 -0400

    Added licence information to the main source file of the project

--- main.c
+++ main.c
@@ -1,3 +1,31 @@
+/*
+
+Copyright (c) 2023 Eugenio Arteaga A.
+
+Permission is hereby granted, free of charge, to any 
+person obtaining a copy of this software and associated 
+documentation files (the "Software"), to deal in the 
+Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to 
+permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice 
+shall be included in all copies or substantial portions
+of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
 #define NULL ((void*)0)
@@ -130,7 +158,6 @@ static long parse(const char* str, long* output)
     int expecting = 1, parens[0xff] = {0}, parencount = 1;
     long out[0xff], outcount = 0, stackcount = 0, unarycount = 0, i = 0, n;
     char unary[0xff], stack[0xff][4], *tok = lex(str, &i);
-
     while (tok) {
         switch (*tok) {
             case 'a' ... 'z':
